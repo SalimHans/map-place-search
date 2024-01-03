72878ed17f2dc2cd1342d8f03796106b2bf9ce0c
HansSalim <salimhans93@gmail.com>
2024-01-03 21:14:55 +0800

    Added listSearchHistory

--- source/redux/state/places/placesSlice.js
+++ source/redux/state/places/placesSlice.js
@@ -3,11 +3,12 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
   isLoading: false,
-  listSearchPlaces: []
+  listSearchPlaces: [],
+  listSearchHistory: []
 }
 
 // Action Thunks
-const fetchPlacesBySearchInput = createAsyncThunk("places/fetchPlaces", async () => {
+const fetchPlacesBySearchInput = createAsyncThunk("places/fetchPlaces", async (searchText = "") => {
   // TODO: We are just using jsonplaceholder for testing
   // TODO: Change this to autocomplete
   const res = await axios("https://jsonplaceholder.typicode.com/users")
@@ -18,12 +19,13 @@ const appConfigSlice = createSlice({
   name: "places",
   initialState,
   extraReducers: (builder) => {
-    builder.addCase(fetchPlacesBySearchInput.pending, (state) => {
+    builder.addCase(fetchPlacesBySearchInput.pending, (state, { meta }) => {
       state.isLoading = true
+      state.listSearchHistory.push(meta?.arg)
     })
-    builder.addCase(fetchPlacesBySearchInput.fulfilled, (state, action) => {
+    builder.addCase(fetchPlacesBySearchInput.fulfilled, (state, { payload }) => {
       state.isLoading = false
-      state.listSearchPlaces = action.payload
+      state.listSearchPlaces = payload
     })
     builder.addCase(fetchPlacesBySearchInput.rejected, (state, action) => {
       state.isLoading = false
