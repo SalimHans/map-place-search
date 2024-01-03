c2953e9239f52c7db513f5a674d8c3418756e152
HansSalim <salimhans93@gmail.com>
2024-01-03 17:53:33 +0800

    Added Mapview on Home screen

--- source/screens/Home/index.js
+++ source/screens/Home/index.js
@@ -1,12 +1,22 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { View } from "react-native"
 
 import styles from "./styles"
+
+import MapView from "react-native-maps"
 
 export default function Home() {
   return (
     <View style={{ flex: 1 }}>
-      <Text>Home</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 3.1319,
+          longitude: 101.6841,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      />
     </View>
   )
 }
