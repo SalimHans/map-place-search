7d11d7e28e9d3f653af5cc0d6d2f5797a1d2acfd
HansSalim <salimhans93@gmail.com>
2024-01-03 21:55:42 +0800

    Clean up on PlaceHistoryRow

--- source/components/rows/PlaceHistoryRow/styles.js
+++ source/components/rows/PlaceHistoryRow/styles.js
@@ -1,6 +1,5 @@
 import { StyleSheet } from "react-native"
 
-import Colors from "~constants/Colors"
 import { normalize } from "~helpers/GlobalUtility"
 
 export default styles = StyleSheet.create({
