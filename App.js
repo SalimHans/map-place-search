84d8e53f23f0d9a2f8321282bc3918de183152c2
HansSalim <salimhans93@gmail.com>
2024-01-03 20:48:15 +0800

    Added Provider on App.js

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from "react"
+import store from "~redux/store"
 
 import { Home } from "~screens"
 
 export default function App() {
-  return <Home />
+  return (
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  )
 }
