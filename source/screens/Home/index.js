23b54f2ffc5374945ab197bb157156e28f3308cd
HansSalim <salimhans93@gmail.com>
2024-01-03 20:31:59 +0800

    Added PlaceCard on Home

--- source/screens/Home/index.js
+++ source/screens/Home/index.js
@@ -4,15 +4,19 @@ import { SafeAreaView, View } from "react-native"
 import MapView from "react-native-maps"
 
 import styles from "./styles"
+import { GlobalStyles } from "~helpers/GlobalStyles"
 import Utils from "~constants/Utils"
 
 import { PlaceSearchList } from "~components/others"
+import { PlaceCard } from "~components/cards"
 
 export default function Home() {
   function renderMapOverlay() {
     return (
       <SafeAreaView style={styles.mapOverlay}>
         <PlaceSearchList />
+        <View style={GlobalStyles.flex} />
+        <PlaceCard style={styles.placeCard} />
       </SafeAreaView>
     )
   }
--- source/screens/Home/styles.js
+++ source/screens/Home/styles.js
@@ -1,10 +1,16 @@
 import { StyleSheet } from "react-native"
 
+import { normalize } from "~helpers/GlobalUtility"
+
 export default styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject
   },
   mapOverlay: {
     flex: 1
+  },
+  placeCard: {
+    marginHorizontal: normalize(12),
+    marginBottom: normalize(12)
   }
 })
