45d0f0f31fcb23c958b4ef288e97bb7679360fbc
HansSalim <salimhans93@gmail.com>
2024-01-03 20:54:28 +0800

    Work on adding handleSearchInputChange on PlaceSearchList

--- source/components/others/PlaceSearchList/index.js
+++ source/components/others/PlaceSearchList/index.js
@@ -1,13 +1,27 @@
-import React from "react"
+import React, { useCallback } from "react"
 import { FlatList, View } from "react-native"
 
+import { debounce } from "lodash"
+import { useDispatch, useSelector } from "react-redux"
+
 import styles from "./styles"
+import { fetchPlacesBySearchInput } from "~redux/state/places/placesSlice"
 
 import { SearchTextInput } from "~components/fields"
 import { PlaceRow } from "~components/rows"
 import HorizontalLineSeparator from "../HorizontalLineSeparator"
 
 export default function PlaceSearchList({}) {
+  const dispatch = useDispatch()
+  const { listSearchPlaces } = useSelector((state) => state.places)
+
+  const searchPlaceDebounce = useCallback(debounce(handleSearchInputChange, 500), [])
+
+  // MARK: Events
+  function handleSearchInputChange(value) {
+    dispatch(fetchPlacesBySearchInput())
+  }
+
   // MARK: Render Methods
   function renderPlaceItem({ item, index }) {
     // TODO
@@ -16,11 +30,11 @@ export default function PlaceSearchList({}) {
 
   return (
     <View style={styles.container}>
-      <SearchTextInput style={styles.searchTextInput} />
+      <SearchTextInput style={styles.searchTextInput} onChange={searchPlaceDebounce} />
       <FlatList
         style={styles.placeFlatList}
         contentContainerStyle={styles.placeFlatListContent}
-        data={[1, 2, 3]} // TODO
+        data={listSearchPlaces}
         keyExtractor={(_, index) => index}
         renderItem={renderPlaceItem}
         ItemSeparatorComponent={<HorizontalLineSeparator />}
