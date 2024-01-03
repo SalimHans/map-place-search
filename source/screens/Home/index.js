8937f7d8276ccc07534a8f4c673f1568c768c3cc
HansSalim <salimhans93@gmail.com>
2024-01-03 20:18:55 +0800

    Work on adding map overlay

--- source/screens/Home/index.js
+++ source/screens/Home/index.js
@@ -1,15 +1,26 @@
 import React from "react"
-import { View } from "react-native"
+import { SafeAreaView, View } from "react-native"
 
 import MapView from "react-native-maps"
 
 import styles from "./styles"
 import Utils from "~constants/Utils"
 
+import { PlaceSearchList } from "~components/others"
+
 export default function Home() {
+  function renderMapOverlay() {
+    return (
+      <SafeAreaView style={styles.mapOverlay}>
+        <PlaceSearchList />
+      </SafeAreaView>
+    )
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <MapView style={styles.map} initialRegion={Utils.DEFAULT_MAP_REGION} />
+      {renderMapOverlay()}
     </View>
   )
 }
--- source/screens/Home/styles.js
+++ source/screens/Home/styles.js
@@ -3,5 +3,8 @@ import { StyleSheet } from "react-native"
 export default styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject
+  },
+  mapOverlay: {
+    flex: 1
   }
 })
