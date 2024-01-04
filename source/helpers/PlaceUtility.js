40ea214692604c461e0e4ca0ff032f96dc5ca024
HansSalim <salimhans93@gmail.com>
2024-01-04 12:28:10 +0800

    Updated constructGooglePhotoUrlByReference to use polyfill

--- source/helpers/PlaceUtility.js
+++ source/helpers/PlaceUtility.js
@@ -1,10 +1,12 @@
 import Config from "react-native-config"
+import { URL } from "react-native-url-polyfill"
 
 function constructGooglePhotoUrlByReference(photoReference, maxWidth = 300) {
-  let baseUrl = new URLSearchParams("https://maps.googleapis.com/maps/api/place/photo")
-  baseUrl.set("maxwidth", maxWidth)
-  baseUrl.set("photo_reference", photoReference)
-  baseUrl.set("key", Config.GOOGLE_MAPS_API_KEY)
+  let baseUrl = new URL("https://maps.googleapis.com/maps/api/place/photo")
+
+  baseUrl.searchParams.append("maxwidth", maxWidth)
+  baseUrl.searchParams.append("photo_reference", photoReference)
+  baseUrl.searchParams.append("key", Config.GOOGLE_MAPS_API_KEY)
 
   return baseUrl
 }
