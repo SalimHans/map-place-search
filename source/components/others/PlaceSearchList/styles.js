9bd602cd330987b161f4b52343f76fe50d75199a
HansSalim <salimhans93@gmail.com>
2024-01-04 14:21:15 +0800

    Clean up on PlaceSearchList

--- source/components/others/PlaceSearchList/styles.js
+++ source/components/others/PlaceSearchList/styles.js
@@ -1,26 +1,10 @@
 import { StyleSheet } from "react-native"
 
-import Colors from "~constants/Colors"
 import { normalize } from "~helpers/GlobalUtility"
 
 export default styles = StyleSheet.create({
   container: {
     paddingHorizontal: normalize(12)
-  },
-  searchTextInput: {
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-    borderBottomWidth: 1,
-    borderBottomColor: Colors.LIGHT_GREY_BORDER
-  },
-  searchHistory: {
-    fontSize: normalize(14),
-    color: Colors.DARK_TEXT_COLOR,
-    paddingHorizontal: normalize(8),
-    paddingVertical: normalize(8)
-  },
-  placeFlatList: {
-    backgroundColor: "white"
   },
   loadingRow: {
     backgroundColor: "white",
