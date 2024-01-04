60ffa220151e7a23d7efc72eb27c91051ff2fc87
HansSalim <salimhans93@gmail.com>
2024-01-04 13:48:25 +0800

    Added AntDesignProvider

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React from "react"
 
 import { Provider } from "react-redux"
+import { Provider as AntDesignProvider } from "@ant-design/react-native"
 import { PersistGate } from "redux-persist/integration/react"
+
 import store, { persistor } from "~redux/store"
 
 import { Home } from "~screens"
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Home />
-      </PersistGate>
-    </Provider>
+    <AntDesignProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Home />
+        </PersistGate>
+      </Provider>
+    </AntDesignProvider>
   )
 }
