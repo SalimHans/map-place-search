e57a112206bd1a4e0ec41b4ee7267f42b656d98a
HansSalim <salimhans93@gmail.com>
2024-01-04 17:11:28 +0800

    Replaced TextInput with TextareaItem on SearchTextInput

--- source/components/fields/SearchTextInput/index.js
+++ source/components/fields/SearchTextInput/index.js
@@ -1,8 +1,9 @@
 import React from "react"
-import { TextInput } from "react-native"
+
+import { TextareaItem } from "@ant-design/react-native"
 
 import styles from "./styles"
 
 export default function SearchTextInput({ style, ...rest }) {
-  return <TextInput placeholder="Search" style={[styles.searchInput, style]} {...rest} />
+  return <TextareaItem placeholder="Search" style={[styles.searchInput, style]} clear {...rest} />
 }
--- source/components/fields/SearchTextInput/styles.js
+++ source/components/fields/SearchTextInput/styles.js
@@ -1,12 +1,7 @@
 import { StyleSheet } from "react-native"
 
-import { normalize } from "~helpers/GlobalUtility"
-
 export default styles = StyleSheet.create({
   searchInput: {
-    backgroundColor: "white",
-    paddingHorizontal: normalize(8),
-    paddingVertical: normalize(8),
-    borderRadius: normalize(6)
+    backgroundColor: "white"
   }
 })
